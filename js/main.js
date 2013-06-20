ada68127235e55e8d35c98e35fca2dfd7bf32506
Andre Hayter <andre.hayter@gmail.com>
2013-06-20 11:45:18 +1000

    added some buttons to switch display styles

--- js/main.js
+++ js/main.js
@@ -1,20 +1,65 @@
 $(function() {
-	var pi = "314159265358979323846264338327950288419716939937510"; // pi
-	var e =  "271828182845904523536028747135266249775724709369995"; // natural 
-	var er = "0.2357111317192329313741434753596167717379838997101"; // primes
-	var mmp = 5;
-	var mme = 2;
-	var piArray = pi.split("");
-	var eArray = e.split("");
-	var erArray = er.split("");
-	var numRings = Math.min(piArray.length, eArray.length);
-	var ringContainer = $('<div id="rings"></div>');
-	var ring;
-	for (var i=0; i < numRings; i++) {
-		ring = $('<div class="ring"></div>');
-		ring.height(piArray[i] * mmp).css('marginBottom', Math.max(erArray[i] * mme, 1) +'px');
-		//ring.height(piArray[i] * mmp).css('marginBottom', '2px');
-		ring.appendTo(ringContainer);
-	}
-	ringContainer.appendTo($('#container'));
+	rings.init();
+	rings.showColoredRings();
+	rings.setupButtons();
 });
+var rings = {
+	pi: 	"314159265358979323846264338327950288419716939937510",
+	e: 		"271828182845904523536028747135266249775724709369995",
+	primes: "012357111317192329313741434753596167717379838997101",
+	colors: ["#FB9B00", "#FBE700", "#BEB32E", "#FDF270", "#F50012", "#A1000C", "#FA6F79", "#0D4FA5", "#04326D", "#6695D1"],
+	piArray: [],
+	eArray: [],
+	primesArray: [],
+	ringHtMultiplier: 5,
+	spaceHtMultiplier: 2,
+	numRings: 0,
+	ringContainer: "",
+	init: function () {
+		this.piArray = this.pi.split("");
+		this.eArray = this.e.split("");
+		this.primesArray = this.primes.split("");
+		this.numRings = Math.min(this.piArray.length, this.eArray.length, this.primesArray.length);
+		this.ringContainer = $('<div id="rings"></div>');
+	},
+	setupButtons: function () {
+		var btn = $('<div class="btn"></div>');
+		var clrRingsBtn = $('<div class="btn clr">Show Colored Rings</div>');
+		clrRingsBtn.appendTo($('#btn-container'));
+		var greyRingsBtn = $('<div class="btn grey">Show Grey Rings</div>');
+		greyRingsBtn.appendTo($('#btn-container'));
+		$('.clr').on('click', rings.showColoredRings);
+		$('.grey').on('click', rings.showGreyRings);
+	},
+	generateRings: function (ht, space, op, clr) {
+		var ring = $('<div class="ring"></div>');
+		ring.height(ht).css({
+			'marginBottom': space + 'px',
+			'opacity': op,
+			'backgroundColor': clr
+		});
+		ring.appendTo(this.ringContainer);
+	},
+	showGreyRings: function() {
+		console.log('show grey rings');
+		$('#rings').empty();
+		for (var i=0; i < rings.numRings; i++) {
+			var ht = rings.piArray[i] * rings.ringHtMultiplier;
+			var space = Math.max(rings.primesArray[i] * rings.spaceHtMultiplier, 1);
+			var opacity = rings.piArray[i] * 0.1;
+			rings.generateRings(20, space, opacity, "#000000");
+		}
+		rings.ringContainer.appendTo($('#container'));	
+	},
+	showColoredRings: function() {
+		$('#rings').empty();
+		for (var i=0; i < rings.numRings; i++) {
+			var ht = rings.piArray[i] * rings.ringHtMultiplier;
+			var space = Math.max(rings.primesArray[i] * rings.spaceHtMultiplier, 1);
+			var color = rings.colors[rings.piArray[i]];
+			rings.generateRings(20, space, 1, color);
+		}
+		rings.ringContainer.appendTo($('#container'));	
+	},
+
+}
