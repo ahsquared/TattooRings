d045096a72ddf467daf3e3592e53b75674db0805
ahsquared <andre.hayter@gmail.com>
2013-06-24 00:02:21 +1000

    modularized the height, spacing and colour changes
    Working to making all settings interchangeable to get lots of variations
    happening

--- js/main.js
+++ js/main.js
@@ -1,70 +1,148 @@
-$(function() {
-	rings.init();
-	rings.showColoredRings();
-	rings.setupButtons();
+$(function () {
+    rings.init();
+    rings.initRings();
+    rings.setupControls();
 });
 var rings = {
-	pi: 	"314159265358979323846264338327950288419716939937510",
-	e: 		"271828182845904523536028747135266249775724709369995",
-	primes: "012357111317192329313741434753596167717379838997101",
-	colors: ["#FB9B00", "#FBE700", "#BEB32E", "#FDF270", "#F50012", "#A1000C", "#FA6F79", "#0D4FA5", "#04326D", "#6695D1"],
-	piArray: [],
-	eArray: [],
-	primesArray: [],
-	ringHtMultiplier: 5,
-	spaceHtMultiplier: 2,
-	numRings: 0,
-	ringContainer: "",
-	init: function () {
-		this.piArray = this.pi.split("");
-		this.eArray = this.e.split("");
-		this.primesArray = this.primes.split("");
-		this.numRings = Math.min(this.piArray.length, this.eArray.length, this.primesArray.length);
-		this.ringContainer = $('<div id="rings"></div>');
-	},
-	setupButtons: function () {
-		$('.clr').on('click', rings.showColoredRings);
-		$('.grey').on('click', rings.showGreyRings);
-		$('.pi-primes').on('click', rings.showPiPrimesRings);
-	},
-	generateRings: function (ht, space, op, clr) {
-		var ring = $('<div class="ring"></div>');
-		ring.height(ht).css({
-			'marginBottom': space + 'px',
-			'opacity': op,
-			'backgroundColor': clr
-		});
-		ring.appendTo(this.ringContainer);
-	},
-	showGreyRings: function() {
-		console.log('show grey rings');
-		$('#rings').empty();
-		for (var i=0; i < rings.numRings; i++) {
-			var ht = rings.piArray[i] * rings.ringHtMultiplier;
-			var space = Math.max(rings.primesArray[i] * rings.spaceHtMultiplier, 1);
-			var opacity = rings.piArray[i] * 0.1;
-			rings.generateRings(20, space, opacity, "#000000");
-		}
-		rings.ringContainer.appendTo($('#container'));	
-	},
-	showColoredRings: function() {
-		$('#rings').empty();
-		for (var i=0; i < rings.numRings; i++) {
-			var ht = rings.piArray[i] * rings.ringHtMultiplier;
-			var space = Math.max(rings.primesArray[i] * rings.spaceHtMultiplier, 1);
-			var color = rings.colors[rings.piArray[i]];
-			rings.generateRings(20, space, 1, color);
-		}
-		rings.ringContainer.appendTo($('#container'));	
-	},
-	showPiPrimesRings: function() {
-		$('#rings').empty();
-		for (var i=0; i < rings.numRings; i++) {
-			var ht = rings.piArray[i] * rings.ringHtMultiplier;
-			var space = Math.max(rings.primesArray[i] * rings.spaceHtMultiplier, 1);
-			var color = rings.colors[rings.piArray[i]];
-			rings.generateRings(ht, space, 1, "#000000");
-		}
-		rings.ringContainer.appendTo($('#container'));	
-	},
+    pi: "314159265358979323846264338327950288419716939937510",
+    e: "271828182845904523536028747135266249775724709369995",
+    primes: "012357111317192329313741434753596167717379838997101",
+    colors: ["#FB9B00", "#FBE700", "#BEB32E", "#FDF270", "#F50012", "#A1000C", "#FA6F79", "#0D4FA5", "#04326D", "#6695D1"],
+    piArray: [],
+    eArray: [],
+    primesArray: [],
+    ringHtMultiplier: 5,
+    spaceHtMultiplier: 2,
+    numRings: 0,
+    ringContainer: "",
+    init: function () {
+        this.piArray = this.pi.split("");
+        this.eArray = this.e.split("");
+        this.primesArray = this.primes.split("");
+        this.numRings = Math.min(this.piArray.length, this.eArray.length, this.primesArray.length);
+    },
+    initRings: function () {
+        this.ringContainer = $('<div id="rings"></div>');
+        this.generateRings();
+        this.changeHeight();
+        this.changeSpacing();
+        this.changeColors();
+    },
+    setupControls: function () {
+        $('#height, #height-amount, #height-multiplier').on('change', function () {
+            rings.changeHeight();
+        });
+        $('#spacing, #space-amount, #space-multiplier').on('change', function () {
+            rings.changeSpacing();
+        });
+        $('#colors, #single-color').on('change', function () {
+            rings.changeColors();
+        });
+    },
+    generateRings: function (ht, space, op, clr) {
+        for (var i = 0; i < rings.numRings; i++) {
+            var ring = $('<div class="ring"></div>');
+            ring.appendTo(this.ringContainer);
+        }
+        this.ringContainer.appendTo($('#container'));
+    },
+    changeHeight: function () {
+        var val = $('#height').val();
+        var ht = parseInt($('#height-amount').val());
+        var mult = parseInt($('#height-multiplier').val());
+        switch (val) {
+            case "even":
+                ht = ht;
+                break;
+            case "e":
+                ht = this.eArray;
+                break;
+            case "pi":
+                ht = this.piArray;
+                break;
+            case "primes":
+                ht = this.primesArray;
+                break;
+        }
+        if (Array.isArray(ht)) {
+            for (var i = 0; i < this.numRings; i++) {
+                $('.ring').eq(i).height(ht[i] * mult);
+            }
+        } else {
+            $('.ring').height(ht * mult);
+        }
+
+    },
+    changeSpacing: function () {
+        var val = $('#spacing').val();
+        var space = parseInt($('#space-amount').val());
+        var mult = parseInt($('#space-multiplier').val());
+        switch (val) {
+            case "even":
+                space =  space;
+                break;
+            case "e":
+                space = this.eArray;
+                break;
+            case "pi":
+                space = this.piArray;
+                break;
+            case "primes":
+                space = this.primesArray;
+                break;
+        }
+        if (Array.isArray(space)) {
+            for (var i = 0; i < this.numRings; i++) {
+                $('.ring').eq(i).css({
+                    "marginBottom": Math.max(space[i] * mult, 1) + "px"
+                });
+            }
+        } else {
+            $('.ring').css({
+                "marginBottom": Math.max(space * mult, 1) + "px"
+            });
+        }
+
+    },
+    changeColors: function (val) {
+        var val = $('#colors').val();
+        var color;
+        switch (val) {
+            case "black":
+                color = "#000000"
+                break;
+            case "grey":
+                color = this.eArray;
+                break;
+            case "single":
+                color = $('#single-color').val();
+                break;
+            case "color":
+                color = this.colors;
+                break;
+        }
+        if (Array.isArray(color) && val === "color") {
+            for (var i = 0; i < this.numRings; i++) {
+                var clr = this.colors[this.piArray[i]];
+                $('.ring').eq(i).css({
+                    "backgroundColor": clr,
+                    "opacity": 1
+                });
+            }
+        } else if (Array.isArray(color) && val === "grey") {
+            for (var i = 0; i < this.numRings; i++) {
+                var opacity = (this.piArray[i] * 0.1) + 0.1;
+                $('.ring').eq(i).css({
+                    "backgroundColor": "#000000",
+                    "opacity": opacity
+                });
+            }
+        } else {
+            $('.ring').css({
+                "backgroundColor": color,
+                "opacity": 1
+            });
+        }
+
+    },
 }
